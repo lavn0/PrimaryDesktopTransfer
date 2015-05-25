78908b417137871e06f65289f0930a2f8fd35761
LAVN <rairairakurakulilac@hotmail.co.jp>
2015-05-25 22:42:20 +0900

    ・アイコン追加 ・タスクトレイ表示追加 ・タスクトレイコンテキストメニュー（終了）追加

--- PrimaryDesktopTransfer/Form1.Designer.cs
+++ PrimaryDesktopTransfer/Form1.Designer.cs
@@ -29,11 +29,39 @@
 		private void InitializeComponent()
 		{
 			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+			this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.SuspendLayout();
+			// 
+			// notifyIcon
+			// 
+			this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+			this.notifyIcon.Text = "PrimaryDesktopTransfer";
+			this.notifyIcon.Visible = true;
+			this.notifyIcon.Click += new System.EventHandler(this.notifyIcon_Click);
+			// 
+			// contextMenuStrip
+			// 
+			this.contextMenuStrip.Name = "contextMenuStrip1";
+			this.contextMenuStrip.Size = new System.Drawing.Size(61, 4);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "Form1";
+			this.ClientSize = new System.Drawing.Size(284, 261);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "PrimaryDesktopTransfer";
+			this.Text = "PrimaryDesktopTransfer";
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.NotifyIcon notifyIcon;
+		private System.Windows.Forms.ContextMenuStrip contextMenuStrip;
 	}
 }
 
--- PrimaryDesktopTransfer/Form1.cs
+++ PrimaryDesktopTransfer/Form1.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace PrimaryDesktopTransfer
@@ -15,6 +9,48 @@ namespace PrimaryDesktopTransfer
 		public Form1()
 		{
 			InitializeComponent();
+		}
+
+		private void 終了するToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+
+		private void notifyIcon_Click(object sender, EventArgs e)
+		{
+			this.contextMenuStrip.Items.Clear();
+			var endMenu = new ToolStripMenuItem("終了する");
+			endMenu.Click += this.終了するToolStripMenuItem_Click;
+			this.contextMenuStrip.Items.Add(endMenu);
+
+			for (var i = 0; i < Screen.AllScreens.Length; i++)
+			{
+				var menu = new ToolStripMenuItem(string.Format("Screen{0}に表示", i));
+				if (Screen.AllScreens[i] == Screen.PrimaryScreen)
+				{
+					menu.Enabled = false;
+					menu.Text += " (メインディスプレイ)";
+				}
+
+				menu.Tag = i;
+				menu.Click += this.Screenに表示ToolStripMenuItem_Click;
+				this.contextMenuStrip.Items.Add(menu);
+			}
+
+			this.contextMenuStrip.Show(Cursor.Position);
+		}
+
+		private void Screenに表示ToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			var strip = sender as ToolStripMenuItem;
+			int screenIndex = (int)strip.Tag;
+
+			var subdisplay = Screen.AllScreens.ElementAt(screenIndex);
+			this.Left = subdisplay.Bounds.X;
+			this.Top = subdisplay.Bounds.Y;
+			this.WindowState = FormWindowState.Normal;
+			this.FormBorderStyle = FormBorderStyle.None;
+			this.WindowState = FormWindowState.Maximized;
 		}
 	}
 }
